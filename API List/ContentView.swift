101396575fbbb767c8294ef7da668ef1c6c0c1f9
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-03-05 09:57:36 -0600

    Finished setting up alert

--- API List/ContentView.swift
+++ API List/ContentView.swift
@@ -8,17 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var categories = [String]()
+    @State private var showingAlert = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List (categories, id: \.self) { category in
+                NavigationLink(destination: Text( category)) {
+                    Text(category)
+                }
+            }
+            .navigationTitle("API Catigories")
         }
-        .padding()
+        .task {
+            await getCategories()
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Loading Error"),
+            message: Text("There was a problem loading the API Categories"),
+                  dismissButton: .default(Text("OK")))
+        }
     }
+                  
+    func getCategories() async {
+           let query = "https://api.publicapis.org/categories"
+           if let url = URL(string: query) {
+               if let (data, _) = try? await URLSession.shared.data(from: url) {
+                   if let decodedResponse = try? JSONDecoder().decode(Categories.self, from: data) {
+                       categories = decodedResponse.categories
+                       return
+                   }
+               }
+           }
+        showingAlert = true
+       }
 }
 
 #Preview {
     ContentView()
 }
+
+struct Categories: Codable {
+    var categories: [String]
+}
+
